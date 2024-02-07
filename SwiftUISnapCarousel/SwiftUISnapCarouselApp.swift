adbd19e3e4fd9d6e066e5c5ede6cdf87808f59ec
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2024-02-07 08:40:15 +0100

    Initial Commit

---
+++ SwiftUISnapCarousel/SwiftUISnapCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISnapCarouselApp.swift
+//  SwiftUISnapCarousel
+//
+//  Created by Kaori Persson on 2024-02-07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISnapCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
